af603fb8ad31008fdd922cab26eb835d98513924
irene <irenemumbe5@gmail.com>
2025-03-26 21:30:02 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ const apiUrl =
   "https://jobicy.com/api/v2/remote-jobs?count=20&geo=usa&industry=marketing&tag=seo";
 const jobListingsContainer = document.getElementById("job-listings");
 const jobDetailsContainer = document.getElementById("job-details");
+const searchBar = document.getElementById("search-bar");
