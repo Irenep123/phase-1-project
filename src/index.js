b3a18fdc1190eeba3f38621516ba97f20d1d6e09
irene <irenemumbe5@gmail.com>
2025-03-26 23:28:49 +0300

    latest update

--- src/index.js
+++ src/index.js
@@ -4,3 +4,25 @@ const jobListingsContainer = document.getElementById("job-listings");
 const jobDetailsContainer = document.getElementById("job-details");
 const searchBar = document.getElementById("search-bar");
 const toggleThemeButton = document.getElementById("toggle-theme");
+
+//to fetch data
+async function fetchJobs() {
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    if (data.jobs) {
+      displayJobs(data.jobs);
+    } else {
+      console.error("No jobs found in the response.");
+    }
+  } catch (error) {
+    console.error("Error fetching job data:", error);
+  }
+}
